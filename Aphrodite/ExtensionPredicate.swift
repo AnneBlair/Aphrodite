fb1d8c1a0b83b4c22b640113b19a1fbfc5ce1790
yinyuguang <291181204@qq.com>
2017-10-09 14:24:33 +0800

    predicate

---
+++ Aphrodite/ExtensionPredicate.swift
@@ -0,0 +1,15 @@
+//
+//  ExtensionPredicate.swift
+//  Aphrodite
+//
+//  Created by AnneBlair on 2017/10/2.
+//  Copyright © 2017年 blog.aiyinyu.com. All rights reserved.
+//
+
+import Foundation
+
+public extension NSPredicate {
+//    func dateToCurrently(date: Date, keyDate: AnyObject) -> NSPredicate {
+//        return NSPredicate(format: "%K > %@", #keyPath(keyDate), Date() as NSDate)
+//    }
+}
